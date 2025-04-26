dbbfcab087090719805ae478b1a0cde66bd8a5c1
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-04-26 21:11:39 +0100

    links test

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ContactForm from "@/components/form/form";
-import ContactPageNavBar from "../../components/NavBars/ContactPageNavBar";
-import ContactPageFooter from "../../components/Footers/contactPageFooter";
+// import ContactPageNavBar from "../../components/NavBars/ContactPageNavBar";
+// import ContactPageFooter from "../../components/Footers/contactPageFooter";
 
 const ContactPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-[#222]">
-      <ContactPageNavBar />
+      {/* <ContactPageNavBar /> */}
       <ContactForm />
-      <ContactPageFooter />
+      {/* <ContactPageFooter /> */}
     </div>
   );
 };
--- src/components/NavBars/ContactPageNavBar.tsx
+++ src/components/NavBars/ContactPageNavBar.tsx
@@ -1,26 +1,26 @@
-import CustomNavBar from "../NavBars/customNavBar";
-import { ArrowLeftIcon } from "@heroicons/react/solid";
-const ContactPageNavBar = () => {
-  return (
-    <CustomNavBar
-      brandHref="/"
-      brandContent={
-        <div className="bg-yellow-500 inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 ">
-          <ArrowLeftIcon className="w-4 h-4 ml-1 text-black" />
-          <p className="text-black">Portfolio</p>
-        </div>
-      }
-      navLinks={[
-        { href: "https://github.com/Slav666", text: "GitHub" },
-        {
-          href: "https://www.linkedin.com/in/slawomir-dyk-b35ab1177/",
-          text: "Linkedin",
-        },
-      ]}
-      showPhone
-      phoneNumber="07562760261"
-    />
-  );
-};
+// import CustomNavBar from "../NavBars/customNavBar";
+// import { ArrowLeftIcon } from "@heroicons/react/solid";
+// const ContactPageNavBar = () => {
+//   return (
+//     <CustomNavBar
+//       brandHref="/"
+//       brandContent={
+//         <div className="bg-yellow-500 inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 ">
+//           <ArrowLeftIcon className="w-4 h-4 ml-1 text-black" />
+//           <p className="text-black">Portfolio</p>
+//         </div>
+//       }
+//       navLinks={[
+//         { href: "https://github.com/Slav666", text: "GitHub" },
+//         {
+//           href: "https://www.linkedin.com/in/slawomir-dyk-b35ab1177/",
+//           text: "Linkedin",
+//         },
+//       ]}
+//       showPhone
+//       phoneNumber="07562760261"
+//     />
+//   );
+// };
 
-export default ContactPageNavBar;
+// export default ContactPageNavBar;
