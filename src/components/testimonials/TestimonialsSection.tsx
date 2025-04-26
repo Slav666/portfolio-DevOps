31482ffa549950aaed642370380eeb1e56c08a65
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-04-26 22:40:22 +0100

    test 5

--- src/components/testimonials/TestimonialsSection.tsx
+++ src/components/testimonials/TestimonialsSection.tsx
@@ -5,17 +5,19 @@ import { testimonials } from "../../constants";
 import TestimonialCard from "./TestimonialCard";
 
 const TestimonialsSection: FC = () => (
-  <div className=" mx-auto py-5 text-center bg-[#222]" id="testimonials">
-    <Title
-      icon={<UsersIcon className="text-blue-500" />}
-      title="Testimonial"
-      iconSize="w-12 h-12"
-      titleSize={{ sm: "text-2xl", lg: "text-4xl" }}
-    />
-    <div className="flex flex-wrap m-4">
-      {testimonials.map((testimonial) => (
-        <TestimonialCard key={testimonial.id} testimonial={testimonial} />
-      ))}
+  <div id="testimonials">
+    <div className=" mx-auto py-5 text-center bg-[#222]">
+      <Title
+        icon={<UsersIcon className="text-blue-500" />}
+        title="Testimonial"
+        iconSize="w-12 h-12"
+        titleSize={{ sm: "text-2xl", lg: "text-4xl" }}
+      />
+      <div className="flex flex-wrap m-4">
+        {testimonials.map((testimonial) => (
+          <TestimonialCard key={testimonial.id} testimonial={testimonial} />
+        ))}
+      </div>
     </div>
   </div>
 );
